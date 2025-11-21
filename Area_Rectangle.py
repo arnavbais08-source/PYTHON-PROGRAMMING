80cc3f384288ea6e7308977e936e7f760381fa35
arnavbais08-source <arnavbais08@gmail.com>
2025-11-21 14:30:13 +0530

    Add files via upload

---
+++ Area_Rectangle.py
@@ -0,0 +1,5 @@
+def arearectangle(l,b):
+    return l*b
+l=int(input("enter the length of the rectangle: "))
+b=int(input("enter the breadth of the rectangle: "))
+print("the area of a rectangle is:  ",arearectangle(l,b))
